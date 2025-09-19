e9f3e2293694b6a59bfb9fc25fbbbf4d5fd6983a
Asipko <avsipko@gmail.com>
2025-09-19 09:36:19 +0300

    add different version deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/pace-calculator/", 
+  base: process.env.VITE_APP_BASE || '/',
 })
