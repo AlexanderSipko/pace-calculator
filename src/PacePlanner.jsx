f52c0356fa6583bc923e4b6512d5d75548e0deaf
Asipko <avsipko@gmail.com>
2025-09-19 11:03:09 +0300

    a

--- src/PacePlanner.jsx
+++ src/PacePlanner.jsx
@@ -100,13 +100,14 @@ export default function PacePlanner() {
     w.focus();
     w.print();
   };
-
+  
   return (
     <div className="max-w-3xl mx-auto p-2 space-y-6 text-gray-900 mt-1 mb-1">
       
       <div className="flex gap-6 justify-center items-center">
+          const env = loadEnv(mode, process.cwd(), '')
         <img src="./athletics.png" alt="Описание" className="w-4 h-4 opacity-80" />
-        <h1 className="text-1xl font-bold text-center text-gray-600 m-0 flex justify-center">расчет темпа</h1>
+        <h1 className="text-1xl font-bold text-center text-gray-600 m-0 flex justify-center">расчет темпа {import.meta.env.VITE_MODE}</h1>
       </div>
       
       <PaceControls
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: process.env.VITE_APP_BASE || '/',
+export default defineConfig(({ mode }) => {
+  // грузим env для указанного режима
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  console.log("⚡ Build mode:", mode)
+  console.log("⚡ Base path:", env.VITE_BASE)
+
+  return {
+    plugins: [react(), tailwindcss(), ],
+    base: env.VITE_BASE || '/',
+  }
 })
