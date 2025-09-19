9470433dd429be0c8ee9615c4fbe69a5cdac0c37
Asipko <avsipko@gmail.com>
2025-09-19 19:27:36 +0300

    add ico and change image

--- src/PacePlanner.jsx
+++ src/PacePlanner.jsx
@@ -106,7 +106,7 @@ export default function PacePlanner() {
     <div className="max-w-3xl mx-auto p-2 space-y-6 text-gray-900 mt-1 mb-1">
       
       <div className="flex gap-6 justify-center items-center">
-        <img src="./athletics.png" alt="Описание" className="w-4 h-4 opacity-80" />
+        <img src="./icons8-run-32.png" alt="Описание" className="w-4 h-4 opacity-80" />
         <h1 className="text-1xl font-bold text-center text-gray-600 m-0 flex justify-center">расчет темпа 
           <span className="text-gray-500 ml-8 text-[10px]">{import.meta.env.VITE_MODE}</span>
         </h1>
