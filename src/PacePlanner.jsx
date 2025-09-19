f03f5f43f975aa468e99d6c472b1f7308407e8df
Asipko <avsipko@gmail.com>
2025-09-19 11:24:56 +0300

    a

--- src/PacePlanner.jsx
+++ src/PacePlanner.jsx
@@ -100,14 +100,16 @@ export default function PacePlanner() {
     w.focus();
     w.print();
   };
-  
+
+
   return (
     <div className="max-w-3xl mx-auto p-2 space-y-6 text-gray-900 mt-1 mb-1">
       
       <div className="flex gap-6 justify-center items-center">
-          const env = loadEnv(mode, process.cwd(), '')
         <img src="./athletics.png" alt="Описание" className="w-4 h-4 opacity-80" />
-        <h1 className="text-1xl font-bold text-center text-gray-600 m-0 flex justify-center">расчет темпа {import.meta.env.VITE_MODE}</h1>
+        <h1 className="text-1xl font-bold text-center text-gray-600 m-0 flex justify-center">расчет темпа 
+          <span className="text-gray-500 ml-10">{import.meta.env.VITE_MODE}</span>
+        </h1>
       </div>
       
       <PaceControls
